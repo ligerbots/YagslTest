cdb57affa0936f254f3c1b78350dc959fbdb8d4f
Paul Rensing <prensing@ligerbots.org>
2024-06-23 18:12:58 -0400

    use encoder scales from code instead of JSON

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -71,11 +71,7 @@ public class SwerveSubsystem extends SubsystemBase {
         // objects being created.
         SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
         try {
-            swerveDrive = new SwerveParser(directory).createSwerveDrive(MAX_SPEED);
-            // Alternative method if you don't want to supply the conversion factor via JSON
-            // files.
-            // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed,
-            // angleConversionFactor, driveConversionFactor);
+            swerveDrive = new SwerveParser(directory).createSwerveDrive(MAX_SPEED, angleConversionFactor, driveConversionFactor);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
